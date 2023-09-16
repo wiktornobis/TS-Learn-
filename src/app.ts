f16d5bfce5cd0826c9c72ebbad9a45d9c51920f3
wiktor <nobis171@wp.pl>
2023-09-16 13:25:34 +0200

    first step todo ts

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,33 @@
-const buttonElement = document.querySelector("button");
+//  <li>
+//     <label for="task-1">Wyrzucić śmieci</label>
+//     <input type="checkbox" id="task-1" name="Wyrzućić śmieci" />
+// </li>
 
-const calculatePrice = (orginalPrice: number, hasDiscount: boolean) => {
-    return hasDiscount ? orginalPrice * 0.8 : orginalPrice;
+
+// Wyrzucenie śmieci
+// Pójść na siłownie
+// Nakarmić psa
+const taskNameInputElement: HTMLInputElement = document.querySelector("#name");
+const addButtonElement: HTMLButtonElement = document.querySelector("button")
+const tasksContainerElement: HTMLElement = document.querySelector(".tasks");
+
+const tasks: string[] = ["Wyrzucenie śmieci", "Pójść na siłownie", "Nakarmić psa"];
+
+const render = () => {
+    tasksContainerElement.innerHTML = " ";
+    tasks.forEach(task => {
+        const taskElement: HTMLElement = document.createElement("li");
+        taskElement.innerText = task;
+        tasksContainerElement.appendChild(taskElement);
+    })
 }
-buttonElement.addEventListener("click", () => {
-    const originalPrice: number = 50;
-    const hasDiscount: boolean = new URLSearchParams(window.location.search).get("discount") === "true"
-    console.log(calculatePrice(originalPrice, hasDiscount))
+
+const addTask = (task: string) => {
+    tasks.push(task);
+}
+addButtonElement.addEventListener("click", (e: Event) => {
+    e.preventDefault();
+    addTask(taskNameInputElement.value);
+    render();
 })
+render();
