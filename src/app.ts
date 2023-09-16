a54f4e9eecdae0823d4cf5cf3d3acf05aa8f355e
wiktor <nobis171@wp.pl>
2023-09-16 12:38:28 +0200

    uninon type

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,6 @@
-let age: number = 29;
-age = 30;
-let ageAsString: string;
-ageAsString = `${age}`;
 
-console.log(ageAsString);
-
-// const add = (v1: number, v2: number) => v1 + v2;
-
-// console.log(add(10, 5));
-
-const input1Element: HTMLInputElement = document.querySelector("#input1");
-const input2Element: HTMLInputElement = document.querySelector("#input2");
-const addBtn = document.querySelector("button");
-
-const add = (v1: number, v2: number) => v1 + v2;
-
-addBtn.addEventListener("click", () => {
-    const sum = add(Number(input1Element.value), Number(input2Element.value));
-    console.log(sum);
-})
+const LogAge = (age: number | string) => {
+    console.log(`Hej mam ${age} lat!`);
+};
+LogAge(28);
+LogAge('dwanaście')
